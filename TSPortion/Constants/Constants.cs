5239acb62a51d2aa7e8c1471b4f5460df24e43a6
wayne.johnson <wayne.johnson@xero.com>
2018-05-11 15:37:46 +1000

    First.

---
+++ TSPortion/Constants/Constants.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSPortion.Constants
+{
+
+    public static class OtherConstants
+    {
+        public const byte SyncByteValue = 0x47;
+    }
+
+    public static class Calculations
+    {
+        public static int BytesToMegs(decimal numberOfBytes)
+        {
+            return (int)numberOfBytes / 1024 / 1024;
+        }
+    }
+
+    public static class Converters {
+        public static string IntByteToBinaryString(int byt)
+        {
+            return Convert.ToString(byt, 2).PadLeft(8, '0');
+        }
+
+        public static string IntShortToBinaryString(short bytes)
+        {
+            return Convert.ToString(bytes, 2).PadLeft(16, '0');
+        }
+
+        public static short ExtractPIDTypeFromTSPacketShort(short bytes)
+        {
+            byte[] bytes16 = BitConverter.GetBytes(bytes);
+            int shift = (int)bytes16[0] << 3;
+            shift = shift >> 3;
+            bytes16[0] = (byte)shift;
+
+            return BitConverter.ToInt16(bytes16, 0);
+
+        }
+
+        public static short BytesToPIDType(int byte1, int byte2)
+        {
+            byte[] bytes = new byte[]
+            {
+                (byte)byte1,
+                (byte)byte2
+            };
+
+            short _16BitShort = BitConverter.ToInt16(bytes, 0);
+            return _16BitShort;
+        }
+
+    }
+
+
+}
---
+++ TSPortion/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using TSPortion.Constants;
+
+namespace TSPortion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int offset = 0;
+
+            string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            string path = String.Format("{0}\\Videos\\20180510_1957 FTS-191-.TS", home.ToString());
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine(OtherConstants.SyncByteValue);
+
+                try
+                {
+                    FileStream stream = new FileStream(path, FileMode.Open);
+
+                    int streamLengthInBytes = (int)stream.Length;
+
+                    Console.WriteLine("Stream length: {0}MB", Calculations.BytesToMegs(stream.Length));
+
+                    StreamReader reader = new StreamReader(stream);
+
+
+                    while (!reader.EndOfStream)
+                    {
+                        int b = reader.Read();
+
+                        if (b == OtherConstants.SyncByteValue)
+                        {
+                            decimal percentageProgessed = ((decimal)offset / (decimal)streamLengthInBytes) * 100;
+                            Console.WriteLine("Offset: {0} / {2} ({3}%) SyncByte {1}", offset, b, streamLengthInBytes, percentageProgessed.ToString("0.0"));
+
+                            int byte2 = reader.Read();
+                            int byte3 = reader.Read();
+                            //watch out for end of file here. Make safe
+
+                            Console.WriteLine($"dec: {byte2} bin:{Converters.IntByteToBinaryString(byte2)} / dec: {byte3} bin: {Converters.IntByteToBinaryString(byte3)}");
+                            short _16bit = Converters.BytesToPIDType(byte2, byte3);
+
+                            Console.WriteLine("Bytes: {0}", Converters.IntShortToBinaryString(_16bit));
+                            Console.WriteLine("PID Could be: {0} and in Hex: {1:X}", Converters.IntShortToBinaryString(Converters.ExtractPIDTypeFromTSPacketShort(_16bit)), Converters.ExtractPIDTypeFromTSPacketShort(_16bit));
+
+                            Console.ReadKey();
+                        }
+
+                        offset++;
+                    }
+
+
+                    stream.Close();
+                }
+                catch (FileLoadException fex)
+                {
+                    Console.WriteLine("File load error: {0}", fex.Message);
+                }
+                catch (IOException ioex)
+                {
+                    Console.WriteLine("I/O error: {0}", ioex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Generic error: {0}", ex.Message);
+                }
+                finally
+                {
+
+                }
+
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
